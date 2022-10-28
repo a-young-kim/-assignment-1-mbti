28a0ed9cfb84026e16dd10d3791785b59f4456a1
ayoung <ayoung7777@gmail.com>
2022-10-28 21:43:30 +0900

    update processbar

--- public/javascripts/move.js
+++ public/javascripts/move.js
@@ -26,18 +26,30 @@ function is_Checked(){
     return true;
 }
 
-// saved in sessionStorage
+// change radio button
 function get_Answer(event){
+
+    if(window.sessionStorage.getItem(event.target.name) == null){
+        // change process bar
+        let processbar = document.getElementById('processbar');
+        let valueNow = processbar.ariaValueNow;
+
+        valueNow = parseInt(valueNow) + 100/12;
+        processbar.style.width = valueNow+'%';
+        processbar.ariaValueNow = valueNow;
+    }
+
+    // saved in sessionStorage
     window.sessionStorage.setItem(event.target.name, event.target.id.split('_')[1]);
 }
 
+
 // move back
 function move_back(){
-    console.log("back");
     history.back();
 }
 
-// 
+// when view change first start
 window.onload = function(){
     let elements = document.getElementsByClassName('form-check-input');
 
@@ -59,6 +71,18 @@ window.onload = function(){
         let answer = window.sessionStorage.getItem(nameOfElement[i]);
         if(answer != null){
             document.getElementById(nameOfElement[i]+'_'+answer).checked = true;
+
+            // change process bar
+            let processbar = document.getElementById('processbar');
+            let valueNow = processbar.ariaValueNow;
+
+            valueNow = parseInt(valueNow) + 100/12;
+
+            if(valueNow > 98){
+                valueNow = 100;
+            }
+            processbar.style.width = valueNow+'%';
+            processbar.ariaValueNow = valueNow;
         }
     }
 };
